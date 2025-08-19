6a84565686fe74b8e3136a97181a494110d9deaa
crRdz <crrrrdz@gmail.com>
2025-08-19 17:46:52 +0800

    Create one-person-one-order logic Test methodology: Use jmeter to send high-concurrency requests as well, and test whether only one order is deducted in the database changes:VoucherOrderServiceImpl Issue：There is a concurrency problem

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -52,7 +52,17 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足");
         }
 
-        //5.扣减库存
+        //5.一人一单
+        Long userId = UserHolder.getUser().getId();
+        //5.1.查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        //5.2.判断是否存在
+        if(count > 0){
+            //用户已经购买过了
+            return Result.fail("用户已经购买过一次");
+        }
+
+        //6.扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock - 1") //set stock = stock - 1
                 .eq("voucher_id", voucherId).gt("stock", 0)// where id = ? and stock > 0
@@ -62,19 +72,18 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             return Result.fail("库存不足");
         }
 
-        //6.创建订单
+        //7.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        //6.1.生成订单id
+        //7.1.生成订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        //6.2.获取当前用户
-        Long userId = UserHolder.getUser().getId();
+        //7.2.获取当前用户
         voucherOrder.setUserId(userId);
-        //6.3.获取代金券id
+        //7.3.获取代金券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
 
-        //7.返回订单id
+        //8.返回订单id
         return Result.ok(orderId);
     }
 }
